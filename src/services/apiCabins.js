2271fc19bda6611ff07db4d10fd0fb2508ba2e2f
Plamena <yomena37@gmail.com>
2024-01-11 23:21:45 +0200

    Install and set up supabase

---
+++ src/services/apiCabins.js
@@ -0,0 +1,12 @@
+import supabase from "./supabase";
+
+export const getCabins = async () => {
+  const { data, error } = await supabase.from("cabins").select("*");
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabins couldn't be loaded");
+  }
+
+  return data;
+};
