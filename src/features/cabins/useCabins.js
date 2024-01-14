fca36d223554cbcf1368487299b4c83c78e37677
Plamena <yomena37@gmail.com>
2024-01-14 16:10:31 +0200

    Abstract react-query into custom hooks

---
+++ src/features/cabins/useCabins.js
@@ -0,0 +1,15 @@
+import { useQuery } from "@tanstack/react-query";
+import { getCabins } from "../../services/apiCabins";
+
+export const useCabins = () => {
+  const {
+    isLoading,
+    data: cabins,
+    error,
+  } = useQuery({
+    queryKey: ["cabins"],
+    queryFn: getCabins,
+  });
+
+  return { isLoading, error, cabins };
+};
