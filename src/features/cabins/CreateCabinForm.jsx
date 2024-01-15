1d60d8e1b9f2fa6101de7b3434dc539eb118ec16
Plamena <yomena37@gmail.com>
2024-01-15 18:02:01 +0200

    Code cleanup

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -6,7 +6,6 @@ import Button from "../../ui/Button";
 import FileInput from "../../ui/FileInput";
 import Textarea from "../../ui/Textarea";
 import FormRow from "../../ui/FormRow";
-
 import { formMessages } from "../../utils/constants";
 import { useCreateCabin } from "./useCreateCabin";
 import { useUpdateCabin } from "./useUpdateCabin";
