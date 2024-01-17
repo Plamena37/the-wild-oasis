fa3297c3a8cbd09f1da34986ffd74a9802469e7b
Plamena <yomena37@gmail.com>
2024-01-17 18:12:31 +0200

    Create second supabase client for signup

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -2,6 +2,14 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabaseStorageKey = import.meta.env.VITE_SUPABASE_STORAGE_KEY;
+
 const supabase = createClient(supabaseUrl, supabaseKey);
+
+export const supabaseSignup = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    storageKey: supabaseStorageKey,
+  },
+});
 
 export default supabase;
