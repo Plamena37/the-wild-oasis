0efe1a19a98d54d4c389c1a4b183dce73291f3be
Plamena <yomena37@gmail.com>
2024-02-11 16:54:31 +0200

    Code cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,4 @@
-import {
-  BrowserRouter,
-  Navigate,
-  Route,
-  Routes,
-  HashRouter,
-} from "react-router-dom";
+import { Navigate, Route, Routes, HashRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
